b70ab46146493bdd56821aa85403992b808fbab8
Bernardo Ianiak <bernardoianiak@gmail.com>
2025-09-01 02:19:40 -0300

    feat: Adiciona erros customizados ao loader.py

--- steamdata/loader.py
+++ steamdata/loader.py
@@ -1,10 +1,10 @@
 import csv
 from .models import Game
-from .exceptions import DataLoadError
+from .exceptions import DataLoadError, DataParseError
 
 class GameLoader:
     """
-    Responsável por carregador jogos a partir de um arquivo CSV.
+    Responsável por carregar jogos a partir de um arquivo CSV.
     """
     @staticmethod
     def load_from_csv(filepath):
@@ -13,15 +13,20 @@ class GameLoader:
             with open(filepath, encoding='utf-8') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    game = Game(
-                        id=int(row['AppID']),
-                        name=row['Name'],
-                        year=int(row['Release date'][-4:]) if row['Release date'] else None,
-                        is_free=float(row['Price']) == 0.0 if row['Price'] else False,
-                        price=float(row['Price']) if row['Price'] else 0.0,
-                        genre=row['Genres'].split(',')
-                    )
+                    try:
+                        game = Game(
+                            id=int(row['AppID']),
+                            name=row['Name'],
+                            year=int(row['Release date'][-4:]) if row['Release date'] else None,
+                            is_free=float(row['Price']) == 0.0 if row['Price'] else False,
+                            price=float(row['Price']) if row['Price'] else 0.0,
+                            genre=row['Genres'].split(',')
+                        )
+                    except Exception as e:
+                        raise DataParseError(f"Erro ao interpretar linha: {row} - {e}")
                     games.append(game)
+        except (OSError, IOError) as e:
+            raise DataLoadError(f"Erro ao abrir arquivo: {e}")
         except Exception as e:
             raise DataLoadError(f"Erro ao carregar jogos: {e}")
         return games
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,4 +2,4 @@ from steamdata.loader import GameLoader
 
 games = GameLoader.load_from_csv('samples/sample_20.csv')
 print(f'Total de jogos carregados: {len(games)}')
-print('Primeiro jogo:', vars(games[2]))
+print('Primeiro jogo:', vars(games[0]))
