b4bce96319377db506297e11a098c7f2cbf42763
Bernardo Ianiak <bernardoianiak@gmail.com>
2025-09-01 02:13:01 -0300

    feat: Adiciona novas exceptions

--- steamdata/exceptions.py
+++ steamdata/exceptions.py
@@ -3,3 +3,15 @@ class DataLoadError(Exception):
     Exceção levantada em caso de erro ao carregar dados.
     """
     pass
+
+class DataParseError(Exception):
+    """
+    Exceção levantada em caso de erro ao interpretar/converter dados do CSV.
+    """
+    pass
+
+class QueryError(Exception):
+    """
+    Exceção levantada em caso de erro ao executar uma consulta.
+    """
+    pass
